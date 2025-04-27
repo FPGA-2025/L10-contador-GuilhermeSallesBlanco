82ae74b4d660c34a22c4c68cfb815930a1969d73
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail.com>
2025-04-27 02:44:02 -0300

    Adicionando tarefas realizadas

--- contador.v
+++ contador.v
@@ -8,6 +8,13 @@ module Contador (
     output reg [7:0] saida
 );
 
-// Insira seu código aqui
+    always @(posedge clk) begin
+        if(!rst_n) begin // Se reset = 0, reinicia o contador no valor 106
+            saida = 8'h6A; // 106 em hexadecimal
+        end 
+        else if(acrescer && !decrecer) saida = saida + 1; // Se acrescer = 1 e decrecer = 0, soma 1 ao contador
+        else if(!acrescer && decrecer) saida = saida - 1; // Se acrescer = 0 e decrecer = 1, subtrai 1 do contador
+        else saida = saida;
+    end
 
 endmodule
